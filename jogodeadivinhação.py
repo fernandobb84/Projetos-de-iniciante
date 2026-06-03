7af550ff1a7a26a3cf317aeecb4f292c7f8c7093
fernandobb84 <luisfernandolulu44@gmail.com>
2026-06-03 09:44:00 -0300

    Update jogodeadivinhação.py

--- jogodeadivinhação.py
+++ jogodeadivinhação.py
@@ -1,23 +1,23 @@
-# import random
-# #jogo de adivinha 
+import random
+#jogo de adivinha 
 
-# num_tentativas = 3
+num_tentativas = 3
 
-# num_maquina = random.randint(1,10)
+num_maquina = random.randint(1,10)
 
-# while True:
+while True:
     
-#     num_player = int(input(f"{'='*30}\nAdivinhe entre 1 e 10\n{'='*30}\nDigite o numero: ")) 
+     num_player = int(input(f"{'='*30}\nAdivinhe entre 1 e 10\n{'='*30}\nDigite o numero: ")) 
     
-#     if num_maquina == num_player:
-#         print("VOCÊ ACERTOU O NUMERO")
-#         break
+    if num_maquina == num_player:
+         print("VOCÊ ACERTOU O NUMERO")
+         break
         
-#     else:   
-#         print(f"VOCE ERROU CONTINUE,VOCE SO TEM MAIS {num_tentativas - 1} TENTATIVAS")
-#         num_tentativas -= 1
-#         if num_tentativas == 0:
-#             print("Suas tentativas acabaram")
-#             break
+     else:   
+        print(f"VOCE ERROU CONTINUE,VOCE SO TEM MAIS {num_tentativas - 1} TENTATIVAS")
+         num_tentativas -= 1
+         if num_tentativas == 0:
+             print("Suas tentativas acabaram")
+             break
 
 
